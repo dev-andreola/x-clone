3602844666facede630d0900fa6a606f8507c2e2
vtAndreola <dev.andreola@gmail.com>
2024-06-17 20:31:04 -0300

    feat: initial layout

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "X",
+  description: "Faça posts, faça amigos e se conecte com o mundo!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>
+        <div className="bg-black h-full">
+          <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+            <div className="grid grid-cols-4 h-full">
+              <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+                {children}
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
