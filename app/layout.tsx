b4692d824df3be9a01c55615ac8d4044318fcc73
vtAndreola <dev.andreola@gmail.com>
2024-06-17 20:47:47 -0300

    feat: initial sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Sidebar from "./components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
         <div className="bg-black h-full">
           <div className="container h-full mx-auto xl:px-30 max-w-6xl">
             <div className="grid grid-cols-4 h-full">
+              <Sidebar />
               <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
                 {children}
               </div>
