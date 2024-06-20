97043e954edc926a3808db292d9b830e6fea54e8
vtAndreola <dev.andreola@gmail.com>
2024-06-19 21:48:02 -0300

    feat: create register modal

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+import { useCallback, useState } from "react";
+import Input from "../Input";
+import Modal from "../Modal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
+const RegisterModal = () => {
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onToggle = useCallback(() => {
+    if (isLoading) {
+      return;
+    }
+
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [isLoading, registerModal, loginModal]);
+
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      registerModal.onClose();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [registerModal]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input
+        placeholder="Nome"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Nome de usuário"
+        onChange={(e) => setUserName(e.target.value)}
+        value={userName}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Senha"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        disabled={isLoading}
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="text-neutral-400 text-center mt-4">
+      <p>
+        Já tem uma conta?
+        <span
+          onClick={onToggle}
+          className="text-white ml-1 cursor-pointer underline hover:opacity-70"
+        >
+          Fazer Login
+        </span>
+      </p>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Cadastre-se"
+      actionLabel="Registrar"
+      onClose={registerModal.onClose}
+      onSubmit={onSubmit}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+};
+
+export default RegisterModal;
